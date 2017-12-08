a4805419f2c7b57ab7c6cc0e3719646b5248ab54
middtown <aarontm9@gmail.com>
2017-12-07 18:35:47 -0700

    included check for winner logic...does not work

--- selector.js
+++ selector.js
@@ -8,12 +8,82 @@ console.log('hello world');
   	//wait for click on game board and pieces
     if ($(this).text()=="") {
       if ((turn%2)==1) { 
-      	$(this).append("X").css(); 
+      	$(this).html("<h2>X</h2>"); 
       	} else { 
-      	$(this).append("O"); }
+      	$(this).html("<h2>O</h2>"); }
+                console.log($(this).html());
       turn++;
+        checkForWinner();
     }
   });
+
+  // check for winner function
+  // The nth-child pseudo-class is specified with a single argument, 
+  //   which represents the pattern for matching elements.
+  function checkForWinner() {
+        console.log("checking for winner");
+      var cell1 = $("#cell1a").html();
+        console.log(cell1);
+      var cell2 = $("#cell1b").html();
+        console.log(cell2);
+      var cell3 = $("#cell1c").html();
+        console.log(cell3);
+      var cell4 = $("#cell2a").html();
+        console.log(cell4);
+      var cell5 = $("#cell2b").html();
+        console.log(cell5);
+      var cell6 = $("#cell2c").html();
+        console.log(cell6);
+      var cell7 = $("#cell3a").html();
+        console.log(cell7);
+      var cell8 = $("#cell3b").html();
+        console.log(cell8);
+      var cell9 = $("#cell3c").html();
+        console.log(cell9);
+    if ((cell1===cell2) && (cell2===cell3)) {
+        //return cell3;
+        console.log(cell3);
+     } else if 
+        ((cell4===cell5) && (cell5===cell6)) {
+        //return cell6;
+                console.log(cell6);
+      }else if 
+        ((cell7===cell8) && (cell8===cell9)) {
+        //return cell9;
+                console.log(cell9);
+      }else if 
+        ((cell1===cell4) && (cell4===cell7)) {
+        //return cell7;
+                console.log(cell7);
+      }else if 
+        ((cell2===cell5) && (cell5===cell8)) {
+        //return cell8;
+                console.log(cell8);
+      }else if 
+        ((cell3===cell6) && (cell6===cell9)) {
+        //return cell9;
+                console.log(cell9);
+      }else if 
+        ((cell1===cell5) && (cell5===cell9)) {
+        //return cell9;
+                console.log(cell9);
+      }else if 
+        ((cell3===cell5) && (cell5===cell7)) 
+        //return cell7;
+                console.log(cell7);
+      // }else
+      //   return ("no winner");
+      // console.log("these cells match");
+      //console.log(cell1);
+      //console.log(cell2);
+      //console.log(cell3);
+    }
+
+ // if (checkForWinner()=="X") { 
+ //        alert("Player 1 wins!"); }
+ //        else { alert("Player 2 wins!"); }
+  
+
 
 // Resent board's text to replay game
 $("#resetButton").click(function() {
